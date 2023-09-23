971172dd8ef8e987fad9610e26187be370270321
Saeeda <saeedabatool39@gmail.com>
2023-09-22 22:37:14 -0400

    implemented apollo server and express

--- Develop/server/server.js
+++ Develop/server/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
@@ -16,6 +17,38 @@ if (process.env.NODE_ENV === 'production') {
 
 app.use(routes);
 
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+const server = new ApolloServer({
+  typeDefs,       
+  resolvers,    
+  context: ({ req }) => {
+    // Retrieve the authorization header from the request
+    const token = req.headers.authorization || '';
+
+    // If there's no token, return an empty context
+    if (!token) {
+      return {};
+    }
+
+    try {
+     
+      const user = jwt.verify(token, 'YOUR_SECRET_KEY');
+
+      return { user };
+    } catch (error) {
+      throw new Error('Authentication failed');
+    }
+  },
 });
+
+server.applyMiddleware({ app });
+
+db.once('open', () => {
+  console.log('Connected to the database');
+
+  // Start the Express server
+  app.listen(PORT, () => {
+    console.log(`🌍 Server is running on http://localhost:${PORT}`);
+    console.log(`🚀 GraphQL is ready at http://localhost:${PORT}${server.graphqlPath}`);
+  });
+});
+
